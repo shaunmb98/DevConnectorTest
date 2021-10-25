5761f693ec1269bebd6e5f03681f38eb993eb69c
shaunmb98 <shaunmb98@gmail.com>
2021-10-25 10:12:40 -0700

    Update

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const app = express();
 app.get('/', (req,res) => res.send('API Running'));
 
 const PORT = process.env.PORT || 5000;
-//
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
