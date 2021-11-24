c7d8262f353549cbc148e60aafb03908bdea3594
Shaun Brown <shaunmb98@gmail.com>
2021-11-23 16:26:05 -0800

    Functionality complete

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -11,11 +11,15 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
         <Link to='/profiles'>Developers</Link>
       </li>
       <li>
+        <Link to='/posts'>Posts</Link>
+      </li>
+      <li>
         <Link to='/dashboard'>
           <i className='fas fa-user' />{' '}
           <span className='hide-sm'>Dashboard</span>
         </Link>
       </li>
+
       <li>
         <a onClick={logout} href='#!'>
           <i className='fas fa-sign-out-alt' />{' '}
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -54,6 +54,7 @@ router.get('/', auth, async (req, res) => {
 router.get('/:id', auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
+    console.log(post);
     if (!post) {
       return res.status(404).json({ msg: 'Post not found' });
     }
